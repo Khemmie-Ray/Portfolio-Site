8098f373b861f0de618b57f50f1552b6d9c215a1
Khemmie-Ray <atokemmy@gmail.com>
2023-01-17 18:03:09 +0100

    update typos and README file

---
+++ README.md
@@ -0,0 +1,27 @@
+## Portfolio Site
+
+A personal portfolio site to showcase my skills, past projects and experiences as a frontend developer who is interested in the potentials of Web3 and blockchain technologies. The design was created to look like a magazine with a coverpage and then the inner pages. I am open to feedback and constructive criticism on how to make this better.
+
+## Resources
+
+* FormSubmit API
+* Mailto links
+
+## Built with
+
+* React
+* CSS flexbox
+* React Routers
+* React Hooks
+* Figma
+
+
+## Links
+
+[Livesite](https://oluwakemi-atoyebi-portfolio.netlify.app/)
+
+[Design]()
+
+## Author
+
+Atoyebi Oluwakemi.
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,9 +6,9 @@ function About() {
        <div className="content about-info">
           <div className="Bio">
             <h3>Who is Oluwakemi?</h3>
-            <p>I am Frontend developer with a solid foundation in building responsive and accessible web applications with frontend technologies such HTML, CSS, JavaScript, React, Bootstrap and TailwindCSS.</p>
-            <p>I am a Web3 developer who is interested in the potentials of blockchain and Web3 technologies to revolutionalize the way we build and use applications for the web.</p>
-            <p className="interests-p">An avid learner who enjoys staying up to date on modern technologies. I go by the nickname Khemmie-Ray and if I am not coding I enjoys music, arts, literature and going on travel adventures.</p>
+            <p>I'm a Frontend developer with a solid foundation in building responsive and accessible web applications with frontend technologies such HTML, CSS, JavaScript, React, Bootstrap and TailwindCSS.</p>
+            <p>I'm a Web3 developer who is interested in the potentials of blockchain and Web3 technologies to revolutionalize the way we build and use applications for the web.</p>
+            <p className="interests-p">An avid learner who enjoys staying up to date on modern technologies. I go by the nickname Khemmie-Ray and if I'm not coding I enjoy music, arts, literature and going on travel adventures.</p>
             <a href="https://www.canva.com/design/DAFVaOGrGWA/wJBf3UOTCOEnWqG1OfDgmA/view?utm_content=DAFVaOGrGWA&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank" className="resume">Resume</a>
           </div>
           <div className="aboutImg">
